d057e96b90ae96f23cd2d7378509a0816907ba3c
aashay15 <57581658+aashay15@users.noreply.github.com>
2020-02-07 19:26:40 +0530

    Add files via upload

---
+++ getkeys.py
@@ -0,0 +1,17 @@
+# Citation: Box Of Hats (https://github.com/Box-Of-Hats )
+
+#to take the key input from the user to feed into the neural network
+
+import win32api as wapi
+import time
+
+keyList = ["\b"]
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
+    keyList.append(char)
+
+def key_check():
+    keys = []
+    for key in keyList:
+        if wapi.GetAsyncKeyState(ord(key)):
+            keys.append(key)
+    return keys
