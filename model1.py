0f0b9a1105109e78c0716d5c79b7170d5ffa6cb1
Aashay Sharma <dellrox442@gmail.com>
2020-03-29 18:58:52 +0530

    changes

--- model1.py
+++ model1.py
@@ -1,4 +1,4 @@
-import tensorflow
+
 import numpy as np
 import tflearn
 from tflearn.layers.core import input_data, fully_connected, dropout
@@ -10,13 +10,13 @@ def multi1():
     #net = fully_connected(net, 5, activation='softsign')
     #net = dropout(net, 0.8)
 
-    net = fully_connected(net, 32, activation='leakyrelu')
+    net = fully_connected(net, 32, activation='relu')
     #net = dropout(net, 0.8)
 
-    net = fully_connected(net, 128, activation='leakyrelu')
+    net = fully_connected(net, 128, activation='relu')
     #net = dropout(net, 0.8)
 
-    net = fully_connected(net, 128, activation='leakyrelu')
+    net = fully_connected(net, 128, activation='relu')
     #net = dropout(net, 0.8)
 
     #net = fully_connected(net, 32, activation='linear')
@@ -24,7 +24,7 @@ def multi1():
 
     net = fully_connected(net, 5, activation='softmax')
 
-    net = regression(net, optimizer='momentum', learning_rate=1e-3, name="targets", loss="categorical_crossentropy")
+    net = regression(net, optimizer='adam', learning_rate=1e-3, name="targets", loss="categorical_crossentropy")
 
     model = tflearn.DNN(net)
 
