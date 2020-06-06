017081b9a81ae41261ce5f58d8acaa7b807adc76
Aashay Sharma <dellrox442@gmail.com>
2020-06-06 14:49:56 +0530

    changes

--- balance_data_model1.py
+++ balance_data_model1.py
@@ -3,11 +3,11 @@ import pandas as pd
 from collections import Counter
 from random import shuffle
 
-train_data = np.load('training_data-1.npy', allow_pickle=True)
+train_data = np.load('extended_data_2.npy ', allow_pickle=True)
 
 df = pd.DataFrame(train_data)
-print(df.head())
-print(Counter(df[1].apply(str)))
+#print(df.head())
+#print(Counter(df[1].apply(str)))
 
 lefts = []
 rights = []
@@ -15,28 +15,37 @@ forwards = []
 backwards = []
 spaces = []
 
+#dists = []
+#keys = []
+
 
 #dists = np.array([i[0] for i in train_data])
 #keys = np.array([j[1] for j in train_data])
 
-
+#print(keys)
 
 shuffle(train_data)
-
+#"""
 for data in train_data:
     dists = data[0]
     keys = data[1]
 
-#print(dists)
+    #print(keys)
 #"""
-for data in train_data:
+#"""
     if keys == [1,0,0,0,0]:
         forwards.append([dists,keys])
     elif keys == [0,1,0,0,0]:
         backwards.append([dists,keys])
     elif keys == [0,0,1,0,0]:
         lefts.append([dists,keys])
+    elif keys == [1,0,1,0,0]:
+        keys = [0,0,1,0,0]
+        lefts.append([dists, keys])
     elif keys == [0,0,0,1,0]:
+        rights.append([dists,keys])
+    elif keys == [1,0,0,1,0]:
+        keys = [0,0,0,1,0]
         rights.append([dists,keys])
     elif keys == [0,0,0,0,1]:
         spaces.append([dists,keys])
@@ -44,7 +53,7 @@ for data in train_data:
         print('no matches')
 
 #print(keys)
-forwards = forwards[:len(lefts)][:len(rights)][:len(backwards)][:len(spaces)]
+forwards = forwards[:len(lefts)]
 lefts = lefts[:len(forwards)]
 rights = rights[:len(forwards)]
 backwards = backwards[:len(forwards)]
@@ -54,6 +63,7 @@ spaces = spaces[:len(forwards)]
 final_data = forwards + lefts + rights + backwards + spaces
 
 shuffle(final_data)
+print(final_data)
+np.save('extended_balanced_2.npy', final_data)
 
-np.save('training_data_v2.npy', final_data)
-#"""
+
--- keras_model_train.py
+++ keras_model_train.py
@@ -1,5 +1,6 @@
 from keras_model import keras_multi
 import numpy as np
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import adam
@@ -12,9 +13,13 @@ model.add(Dense(64, activation = 'relu'))
 #model.add(Dropout(0.5))
 model.add(Dense(128, activation = 'relu'))
 #model.add(Dropout(0.5))
-model.add(Dense(256, activation = 'relu'))
+#model.add(Dense(256, activation = 'relu'))
 
-model.add(Dense(128, activation = 'relu'))
+#model.add(Dense(512, activation = 'relu'))
+
+#model.add(Dense(256, activation = 'relu'))
+
+#model.add(Dense(128, activation = 'relu'))
 
 model.add(Dense(64, activation = 'relu'))
 
@@ -23,15 +28,16 @@ model.add(Dense(32, activation = 'relu'))
 
 
 model.add(Dense(5, activation = 'softmax'))
-
+ADAM = keras.optimizers.Adam(learning_rate= 1e-6, beta_1=0.9, beta_2=0.999, amsgrad=False)
+SGD = keras.optimizers.SGD(learning_rate=0.00001, momentum=0.9, nesterov=False)
 model.compile(loss='categorical_crossentropy',
-                        optimizer = 'adam',
+                        optimizer = ADAM,
                         metrics = ['accuracy'])
 
 #model = keras_multi()
 
 MODEL_NAME = 'KERAS_MLP'
-data = np.load('training_data_v3.npy', allow_pickle=True)
+data = np.load('extended_balanced_2.npy', allow_pickle=True)
 
 train = data[:7000]
 test = data[7000:]
@@ -42,8 +48,11 @@ test_x = np.array([i[0] for i in test])
 test_y = np.array([j[1] for j in test])
 
 model.fit(X,Y,
-          epochs = 40,
+          epochs = 60,
+
           batch_size = 5,
           validation_data = (test_x,test_y))
+keras.callbacks.callbacks.ModelCheckpoint("/Users/aashaysharma/Desktop/Self-driving-car-AI-GTA_SA/model_checkpoints", monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=10)
 
-model.save('keras_model_test.model')
+
+model.save('keras_model_test_6.model')
